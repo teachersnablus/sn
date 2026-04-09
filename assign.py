a99e1b8257d372aa367a9d17a66dc1865338380d
teachersnablus <teachersouthnablus@gmail.com>
2026-04-09 08:14:08 +0300

    Update assign.py

--- assign.py
+++ assign.py
@@ -133,7 +133,7 @@ if not st.session_state['auth']:
                 st.error("❌ فشل الاتصال - يرجى المحاولة مرة أخرى")
     with tab2:
         adm_pass = st.text_input("كلمة مرور الإدارة", type="password")
-        if st.button("دخول المسؤول"):
+        if st.button("دخول القسم"):
             if adm_pass == "ADMIN2026":
                 st.session_state.update({'auth': True, 'user_type': "admin"})
                 st.rerun()
